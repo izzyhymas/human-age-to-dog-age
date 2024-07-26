f79b1a7707e823c48c0f9ac25ca2d508964b4d56
Izzy Hymas <izzyhymas@gmail.com>
2024-07-26 10:15:49 -0600

    Fix background and font to work with GH pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import grassImg from "./assets/grass.png";
+import retro from "./assets/fonts/kindly-rewind-font/KindlyRewind-BOon.ttf";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+document.documentElement.style.setProperty(
+  "--background-image-url",
+  `url(${grassImg})`
+);
+document.documentElement.style.setProperty(
+  "--kindly-rewind-font-url",
+  `url(${retro})`
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
